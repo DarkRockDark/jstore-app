1f4b1f3f261161ca41943a83a17135bb5083a49a
Doug Rogers <drogers@netroadshow.com>
2022-11-22 19:34:09 -0500

    push to correct branch

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React"/>
-      <Todo text="Master React"/>
-      <Todo text="Explore Full react course"/>
+    <h1>Andrea's To-Do list</h1>
+    <Todo text="Continue being awesome"/>
+    <Todo text="Master of the home"/>
+    <Todo text="Get the husband working"/>
     </div>
   );
 }
