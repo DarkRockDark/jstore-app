89ad5de95fa4c004a86dbaaf9efa93344655b2af
Doug Rogers <drogers@netroadshow.com>
2022-11-30 15:04:01 -0500

    fav badge

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,28 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
+
 function FavoritesPage() {
-    return <div>Favorites Page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = (
+      <p>
+        You do not have any favorite Meetups. Add favorites from All Meetups
+        page.
+      </p>
+    );
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+  return (
+    <section>
+      <h2>Favorites Page</h2>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
