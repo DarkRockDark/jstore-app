4f5c19fae7036ab65c0b22cdb32c38e11b5733d8
Doug Rogers <drogers@netroadshow.com>
2022-12-01 12:53:00 -0500

    exp-react start

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 
-import "./index.css";
-import App from "./App";
-import { FavoritesContextProvider } from "./store/favorites-context";
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <FavoritesContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </FavoritesContextProvider>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
